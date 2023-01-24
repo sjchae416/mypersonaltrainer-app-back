be888d32b50e6cae836d6b4b3ecb3e96364aa1e7
Seungjun Chae <sjchae@sjchae-Gram17.localdomain>
2023-01-24 01:08:50 -0500

    open server && connect MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-// NOTE open server
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
 
-// NOTE connect to db
+mongoose.connect(process.env.MongoDB_url, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => {
+	console.log('Connected to Databse');
+});
+
+app.use(express.json());
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
 
 // NOTE create REST API (CRUD)
-
-// NOTE connect app
+// Create a routine
+// Read all routines
+// Read a routine(:id)
+// Update a routine(:id)
+// Delete a routine(:id)
